dae33c7edcf4e25d13499d04c517a58a3cfeec31
dunglt <leetd@ltd-laptop.local>
2024-05-01 12:25:35 +0700

    re-add src

---
+++ api/Controllers/HealthCheckController.cs
@@ -0,0 +1,16 @@
+﻿namespace WebApi.Controllers;
+
+using Microsoft.AspNetCore.Mvc;
+
+[ApiController]
+[Route("api/[controller]")]
+public class HealthCheckController : ControllerBase
+{
+    [HttpGet]
+    public IActionResult Check()
+    {
+        // check database connection, external services, etc.
+        // if everything is fine, return HTTP 200 OK
+        return Ok(new { status = "::: Healthy :::" });
+    }
+}
